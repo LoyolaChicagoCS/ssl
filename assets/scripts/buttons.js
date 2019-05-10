9bbc091074cc8e344e283d152e358e0b6c7eae1e
Morgan Richardson <morganrmarie@gmail.com>
2019-05-10 10:07:17 -0500

    updates: new design, adding member images

---
+++ assets/scripts/buttons.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     $(document).on('mouseenter', '.pics', function () {
+//         $(this).find(":button").show();
+//     }).on('mouseleave', '.pics', function () {
+//         $(this).find(":button").hide();
+//     });
+// });
