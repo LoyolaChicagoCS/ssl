534766540f31d8f6873ae9d46d2611b99f644867
Morgan Richardson <morganrmarie@gmail.com>
2019-05-11 21:27:44 -0500

    updates: adding director's bio and fixing about link

---
+++ assets/scripts/bios.js
@@ -0,0 +1,19 @@
+
+function morganBio() {
+  var bio = document.getElementById('morgan-description');
+  if (bio.style.display== "block") {
+    bio.style.display = 'none';
+  } else {
+    bio.style.display = 'block';
+  }
+}
+
+function seanBio() {
+  var bio = document.getElementById('sean-description');
+  if (bio.style.display== "block") {
+    bio.style.display = 'none';
+  } else {
+    bio.style.display = 'block';
+  }
+}
+}
--- assets/scripts/buttons.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function () {
-//     $(document).on('mouseenter', '.pics', function () {
-//         $(this).find(":button").show();
-//     }).on('mouseleave', '.pics', function () {
-//         $(this).find(":button").hide();
-//     });
-// });
