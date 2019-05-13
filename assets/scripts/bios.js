61b76c31b4b8e3aad5b738fe6ea4b4465c5fb816
Morgan Richardson <morganrmarie@gmail.com>
2019-05-13 13:31:06 -0500

    updates: adding js for bios, adding carousel for projects, adding images

--- assets/scripts/bios.js
+++ assets/scripts/bios.js
@@ -1,19 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const team = [
+        'allan',
+        'zac',
+        'david',
+        'riley',
+        'morgan',
+        'sean',
+        'jack',
+        'jonathan',
+        'emmanuel',
+        'linette',
+        'george'
+    ];
 
-function morganBio() {
-  var bio = document.getElementById('morgan-description');
-  if (bio.style.display== "block") {
-    bio.style.display = 'none';
-  } else {
-    bio.style.display = 'block';
-  }
-}
+    displayBioOnClick(team);
 
-function seanBio() {
-  var bio = document.getElementById('sean-description');
-  if (bio.style.display== "block") {
-    bio.style.display = 'none';
-  } else {
-    bio.style.display = 'block';
-  }
-}
-}
+    function displayBioOnClick(team) {
+        for (let person of team) {
+            let personPhoto = document.getElementById(person);
+            const personBio = document.getElementById(`${person}-description`);
+
+            personPhoto.addEventListener('click', () => {
+                if (personBio.style.display === 'block') {
+                    personBio.style.display = 'none';
+                } else {
+                    personBio.style.display = 'block';
+                }
+            });
+
+            window.addEventListener('click', (event) => {
+                if (event.target !== personPhoto) {
+                    personBio.style.display = 'none';
+                }
+            });
+        }
+    }
+});
