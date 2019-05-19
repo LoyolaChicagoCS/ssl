bd844e931ffdc72aa4f907ef54be52a9a8126e32
Morgan Richardson <morganrmarie@gmail.com>
2019-05-18 22:48:18 -0500

    adding scroll animation

---
+++ assets/scripts/scroll-animation.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('a').click(function() {
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
+});
